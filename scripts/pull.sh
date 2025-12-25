bf426fd683e8158c706b0dbd41d3220ce60794a9
jolenarhasan <163359781+jolenarhasan@users.noreply.github.com>
2025-12-25 21:03:27 +0200

    Update pull.sh

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 PRO_DIR="$(cd "$SCRIPT_DIR/../.." && pwd)"
 PROJECT_DIR="$PRO_DIR/UnixProject"
 
-cd "$PRO_DIR" || { echo "Failed to enter PRO directory"; exit 1; }
+cd "$PRO_DIR" || { echo "Failed to enter directory"; exit 1; }
 
 if [ -d "$PROJECT_DIR/.git" ]; then
     echo "Project exists. Pulling latest changes..."
@@ -17,6 +17,5 @@ else
     git clone https://github.com/jolenarhasan/UnixProject.git
     cd "$PROJECT_DIR" || { echo "Failed to enter project directory"; exit 1; }
 fi
-
 
 echo "Project is up to date at $PROJECT_DIR"
